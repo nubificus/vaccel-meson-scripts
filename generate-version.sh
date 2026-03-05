bffe04881b00990aeb555b1d78cd6e62f7af90ca
Kostis Papazafeiropoulos <papazof@gmail.com>
2026-03-05 22:06:30 +0000

    refactor(generate-version): Improve git dir detection and tag handling
    Improve git directory detection, tag handling and overall
    `generate-version.sh` code:
    - Use `getopt` to parse arguments
    - Use meson source root as the default source directory when called from
      a meson build file
    - Do not fail if the specified directory is not a git repository
    - Add option to output to file
    - Sort tags on the same commit by version metadata and strip version
      metadata for non-tagged commits
    
    PR: https://github.com/nubificus/vaccel-meson-scripts/pull/14
    Signed-off-by: Kostis Papazafeiropoulos <papazof@gmail.com>
    Reviewed-by: Anastassios Nanos <ananos@nubificus.co.uk>
    Approved-by: Anastassios Nanos <ananos@nubificus.co.uk>

--- generate-version.sh
+++ generate-version.sh
@@ -3,37 +3,149 @@
 
 set -e
 
-SRC_DIR=$1
-DEFAULT_VERSION="v0.0.0"
+_SH_SCRIPTS_DIR="$(cd -- "$(dirname -- "$0")" >/dev/null && pwd -P)"
+SH_SCRIPTS_DIR="${SH_SCRIPTS_DIR:-"$_SH_SCRIPTS_DIR"}"
+# shellcheck source=/dev/null
+. "${SH_SCRIPTS_DIR}/_sh-common.sh"
 
-[ -n "${SRC_DIR}" ] || SRC_DIR="."
-
-if [ -f "${SRC_DIR}/.version" ]; then
-	cat "${SRC_DIR}/.version"
-	exit 0
+if [ -n "$MESON_SUBDIR" ]; then
+	_MESON_SOURCE_DIR="${MESON_SOURCE_ROOT}/${MESON_SUBDIR}"
+else
+	_MESON_SOURCE_DIR="$MESON_SOURCE_ROOT"
 fi
 
-if [ ! -d "${SRC_DIR}/.git" ]; then
-	echo ${DEFAULT_VERSION}
-	exit 1
-fi
+SOURCE_DIR_DEFAULT="${_MESON_SOURCE_DIR:-.}"
+VERSION_DEFAULT='v0.0.1'
 
-VERSION=$(git describe --abbrev=8 --tags --match "v[0-9]*" 2>/dev/null || echo)
+parse_args() {
+	short_opts='s:,w,o:'
+	long_opts='source-dir:,write,output-dir:,no-dirty'
 
-if [ -z "${VERSION}" ]; then
-	SHA=$(git describe --abbrev=8 --always 2>/dev/null)
-	COMMITS=$(git log --oneline | wc -l 2>/dev/null)
-	VERSION="v0.0.0-${COMMITS}-g${SHA}"
-fi
-
-DIRTY=$(git diff --quiet || echo '-dirty')
-for i in "$@"; do
-	if [ "$i" = "--no-dirty" ]; then
-		DIRTY=""
-		break
+	if ! getopt=$(getopt -o "$short_opts" --long "$long_opts" \
+		-n "$SH_SCRIPT_NAME" -- "$@"); then
+		sh_error 'Failed to parse args'
 	fi
-done
 
-VERSION=$(echo "${VERSION}${DIRTY}" | sed -e 's/-g/-/' | cut -c 2-)
+	eval set -- "$getopt"
 
-echo "${VERSION}"
+	write=0
+	output_dir=
+	no_dirty=0
+	while true; do
+		case "$1" in
+		'-s' | '--source-dir')
+			# Source directory
+			[ -z "$2" ] &&
+				sh_error "'$1' requires a non-empty string"
+			source_dir="$2"
+			shift 2
+			;;
+		'-w' | '--write')
+			# Write to file
+			write=1
+			shift 1
+			;;
+		'-o' | '--output-dir')
+			# Write to file in output directory
+			[ ! -d "$(dirname "$2")" ] &&
+				sh_error "'$1' directory does not exist"
+			write=1
+			output_dir="$2"
+			shift 2
+			;;
+		'--no-dirty')
+			# Do not append '-dirty' metadata
+			no_dirty=1
+			shift 1
+			;;
+		--)
+			shift
+			break
+			;;
+		*)
+			sh_error 'Internal error parsing args'
+			;;
+		esac
+	done
+
+	source_dir="${source_dir:-"$SOURCE_DIR_DEFAULT"}"
+
+	unset short_opts
+	unset long_opts
+}
+
+generate_version() {
+	dirty=
+
+	if git -C "$source_dir" status >/dev/null 2>&1 &&
+		[ -e "${source_dir}/.git" ]; then
+		if git -C "$source_dir" describe --tags --match "v[0-9]*" \
+			--exact-match HEAD >/dev/null 2>&1; then
+			version=$(git -C "$source_dir" tag --points-at HEAD |
+				grep -E '^v[0-9]' |
+				awk '{
+					tag = $0
+					if (match($0, /\+[^,]*/)) {
+					  base = substr($0, 1, RSTART-1)
+					  build = substr($0, RSTART+1, RLENGTH-1)
+					} else {
+					  base = $0
+					  build = ""
+					}
+					print base "," build "," tag
+				}' |
+				sort -V -t',' -k1,1 -k2,2 |
+				tail -1 |
+				cut -d',' -f3)
+		else
+			version=$(git -C "$source_dir" describe \
+				--abbrev=8 --tags --match "v[0-9]*" 2>/dev/null |
+				sed 's/+[^-]*//' ||
+				true)
+		fi
+
+		if [ -z "$version" ]; then
+			sha=$(git -C "$source_dir" describe --abbrev=8 --always)
+			commits=$(git -C "$source_dir" log --oneline | wc -l)
+			version="v0.0.1-${commits}-${sha}"
+
+			unset sha
+			unset commits
+		fi
+
+		if [ "$no_dirty" -eq 0 ]; then
+			dirty=$(git -C "$source_dir" diff --quiet ||
+				echo '-dirty')
+		fi
+	fi
+
+	version=$(echo "${version:-"$VERSION_DEFAULT"}${dirty}" |
+		sed -e 's/-g/-/' | cut -c 2-)
+
+	unset dirty
+}
+
+main() {
+	parse_args "$@"
+
+	version_file="${source_dir}/.version"
+	if [ -f "$version_file" ]; then
+		cat "$version_file"
+		exit 0
+	fi
+
+	generate_version
+
+	if [ "$write" -eq 0 ]; then
+		echo "$version"
+		exit 0
+	fi
+
+	if [ -z "$output_dir" ]; then
+		echo "$version" >"$version_file"
+	else
+		echo "$version" >"${output_dir}/.version"
+	fi
+}
+
+main "$@"
